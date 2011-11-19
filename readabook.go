30d918e577f83165b9d8a5388feb2d296dcdbd0c
Alex Ray <ajray@ncsu.edu>
2011-11-18 21:37:18 -0500

    ignore files, readabook.go is playing with a reader

---
+++ readabook.go
@@ -0,0 +1,10 @@
+// readabook.go - reads a book for words and frequencies
+// Example: http://www.gutenberg.org/ebooks/2701.txt.utf8
+// TODO(ajray):
+//   * Ignore project gutenberg headers, for now they are parsed
+//		with the rest of the text.
+
+
+package main
+
+
